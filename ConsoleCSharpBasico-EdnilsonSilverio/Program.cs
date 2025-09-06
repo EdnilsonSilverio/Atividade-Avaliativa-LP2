76b7bb22a3258d69292728a7fd14d191997c3790
EdnilsonSilverio <ednilsonsilverio321@gmail.com>
2025-09-05 21:35:33 -0300

    Adição da função DeclararVariaveis para incluir variáveis dos tipos int, float, double, char, string, bool, DateTime

--- ConsoleCSharpBasico-EdnilsonSilverio/Program.cs
+++ ConsoleCSharpBasico-EdnilsonSilverio/Program.cs
@@ -7,16 +7,41 @@ namespace ConsoleCSharpBasico_TeuNome{
 
         static void Main(string[] args)
         {
-            Using();
+            //Using(); Comentado para enfatiza a visualização alvo do Commit
+            DeclararVariaveis();
         }
 
         static void Using()
         {
             Console.WriteLine("Sem o Using System console, esse comando seria: System.Console.WriteLine()");
             Console.WriteLine("Ajudando na organização e legibilidade do código");
-            Console.WriteLine("O Using System.Data permite que eu capture infomações como a data atual: "+
+            Console.WriteLine("O Using System.Data permite que eu capture infomações como a data atual: " +
             DateTime.Now);
             Console.WriteLine("FIM DO USING\n---------------------------------------");
+        }
+        
+        static void DeclararVariaveis()
+        {
+            int idade = 21;
+            float altura = 1.73f;
+            double peso = 73.7;
+            char letra = 'D';
+            string nome = "Ednilson da Silva Silvério";
+            bool aprovado = true;
+            DateTime hoje = DateTime.Now;
+
+            if (aprovado)
+            {
+                Console.WriteLine("Parabéns " + nome + "\nVocê foi aprovado");
+                Console.WriteLine("-----------Dados do aluno aprovado---------------");
+                Console.WriteLine("Idade: " + idade + "\nAltura: " + altura + "\nPeso: " +
+                peso + "\nInicial: " + letra + "\nData e hora da consulta no sistema: " + hoje);
+
+            }
+            else
+            {
+                Console.WriteLine("Você foi reprovado!\nTente novamente!");
+            }
         }
     }
 }  
