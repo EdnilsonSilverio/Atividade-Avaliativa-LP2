e532870b62d7e8ad07b45629da324d565080693c
EdnilsonSilverio <ednilsonsilverio321@gmail.com>
2025-09-05 21:25:35 -0300

    Adição da função Using, evidenciando seu uso e importância no código.

--- ConsoleCSharpBasico-EdnilsonSilverio/Program.cs
+++ ConsoleCSharpBasico-EdnilsonSilverio/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Data;
+
+namespace ConsoleCSharpBasico_TeuNome{
+    class Program{
+
+        static void Main(string[] args)
+        {
+            Using();
+        }
+
+        static void Using()
+        {
+            Console.WriteLine("Sem o Using System console, esse comando seria: System.Console.WriteLine()");
+            Console.WriteLine("Ajudando na organização e legibilidade do código");
+            Console.WriteLine("O Using System.Data permite que eu capture infomações como a data atual: "+
+            DateTime.Now);
+            Console.WriteLine("FIM DO USING\n---------------------------------------");
+        }
+    }
+}  
